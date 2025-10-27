b2e33c0105d07f3469373feafe5687cca0baf0d1
bendoujanna <b.vitalina@alustudent.com>
2025-10-27 21:06:22 +0200

    minor changes

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module retrieves an employee's TODO list from the JSONPlaceholder API
-And exports the data to a CSV file.
+and exports the data to a CSV file.
 
 The script takes an employee ID as a command-line argument and creates a file
 named <employee_id>.csv with the following format:
@@ -13,21 +13,26 @@ import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
     """Main entry point of the script."""
 
     if len(sys.argv) != 2:
         print("Usage: ./1-export_to_CSV.py <employee_id>")
         sys.exit(1)
+
     employee_id = int(sys.argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)    
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        employee_id)
+
     user = requests.get(user_url).json()
     todos = requests.get(todo_url).json()
     username = user.get("username")
 
-    # file name
-    filename = f"{employee_id}.csv"
+    # File name
+    filename = "{}.csv".format(employee_id)
 
     # Write to CSV file
     with open(filename, mode="w", newline="", encoding="utf-8") as csvfile:
@@ -40,4 +45,5 @@ if __name__ == "__main__":
                 task.get("title")
             ])
 
-    print(f"Data exported to {filename}")
+    print("Data exported to {}".format(filename))
+
